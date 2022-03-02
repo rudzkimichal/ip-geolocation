cd474064173e69ca507a78d6306b83c2965acb7e
Michal Rudzki <michal.rudzki@yahoo.com>
2022-03-03 00:01:32 +0100

    refactored api router

--- app/server/routes/apiRouter.ts
+++ app/server/routes/apiRouter.ts
@@ -1,9 +1,15 @@
 import axios from 'axios';
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 export const apiRouter = express.Router();
 
-apiRouter.get(`/api/:item`, async (req: express.Request, resp: express.Response) => {
-    const response = await axios.get(`http://api.ipstack.com/${req.params.item}?access_key=${process.env.API_KEY}`);
-    resp.send(response.data);
+apiRouter.get(`/api/:item`, async (req: Request, resp: Response) => {
+  try {
+    const item = req?.params?.item;
+    const response = await axios.get(`http://api.ipstack.com/${item}?access_key=${process.env.API_KEY}`);
+    resp.status(200).send(response.data);
+
+  } catch(e) {
+    resp.status(404).send('Unable to find item matching query');
+  }
 });
